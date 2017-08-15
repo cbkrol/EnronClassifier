0e38b8d514b9e68dd08e0190687e9486da2bb811
Chantal <cbkrol@gmail.com>
2017-08-15 13:45:25 -0400

    Add files via upload

---
+++ tools3/targetFeatureSplit.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+
+# In[1]:
+
+def targetFeatureSplit( data ):
+    """ 
+        given a numpy array like the one returned from
+        featureFormat, separate out the first feature
+        and put it into its own list (this should be the 
+        quantity you want to predict)
+
+        return targets and features as separate lists
+
+        (sklearn can generally handle both lists and numpy arrays as 
+        input formats when training/predicting)
+    """
+
+    target = []
+    features = []
+    for item in data:
+        target.append( item[0] )
+        features.append( item[1:] )
+
+    return target, features
+
+
+# In[ ]:
+
+
+
